c7e10b1f6abaf133aa3c8d479e4c1add9f3424f3
Rachel Traina-Grandon <rtraina-grandon@hitactics.com>
2017-04-18 15:58:16 -0700

    Initial commit. Moved from single html file into small flask application.

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/technologies')
+def technologies():
+    return render_template("technologies.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
