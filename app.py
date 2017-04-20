fe2e23e9056c23339c00778e414d50bd328a6f88
Rachel Traina-Grandon <rtraina-grandon@hitactics.com>
2017-04-19 21:34:43 -0700

    Changes for Heroku

--- app.py
+++ app.py
@@ -29,4 +29,7 @@ def contact():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run(port=5000, debug=False)
+    # app.run(host="0.0.0.0", port=5000, debug=False)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
