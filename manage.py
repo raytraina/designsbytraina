baf0b9ade291be5f22b3d5b03dfdf2473932b829
Rachel Traina-Grandon <rtraina-grandon@hitactics.com>
2017-04-25 19:17:01 -0700

    Cleaned up styling and code.

--- manage.py
+++ manage.py
@@ -5,33 +5,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render index page."""
     return render_template("index.html")
 
 @app.route('/about')
 def about():
+    """Render about page."""
     return render_template("about.html")
 
 @app.route('/technologies')
 def technologies():
+    """Render skills/tools page."""
     return render_template("technologies.html")
 
 @app.route('/projects')
 def projects():
+    """Render projects page."""
     return render_template("projects.html")
 
 @app.route('/resume')
 def resume():
+    """Render resume page."""
     return render_template("resume.html")
 
 @app.route('/contact')
 def contact():
+    """Render contact page."""
     return render_template("contact.html")
 
 
 
 if __name__ == '__main__':
-
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
-    # app.run(host="0.0.0.0", port=5000, debug=False)
-    # app.run(debug=True, host='0.0.0.0', port=5000)
